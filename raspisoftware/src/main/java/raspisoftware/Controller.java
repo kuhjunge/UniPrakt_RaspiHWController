48b3e6f2d8137936796b125d304479f53ba20a3c
Kuhjunge <quhberta@gmail.com>
2018-04-25 13:50:52 +0200

    pom korrigiert

--- raspisoftware/src/main/java/raspisoftware/Controller.java
+++ raspisoftware/src/main/java/raspisoftware/Controller.java
@@ -13,35 +13,17 @@ public class Controller {
         final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
-
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.HIGH);
+        //pin.high();
+        pin.setState(PinState.HIGH);
+        //pin.setState(true);
         // set shutdown state for this pin
-        pin.setShutdownOptions(true, PinState.LOW);
-
+        //pin.setShutdownOptions(true, PinState.LOW);
+        
 
         try {
-        	 System.out.println("--> GPIO state should be: ON");
-			Thread.sleep(5000);
-			pin.low();
-	        System.out.println("--> GPIO state should be: OFF");
 
-	        Thread.sleep(5000);
-
-	        // toggle the current state of gpio pin #01 (should turn on)
-	        pin.toggle();
-	        System.out.println("--> GPIO state should be: ON");
-
-	        Thread.sleep(5000);
-
-	        // toggle the current state of gpio pin #01  (should turn off)
-	        pin.toggle();
-	        System.out.println("--> GPIO state should be: OFF");
-
-	        Thread.sleep(5000);
-
-	        // turn on gpio pin #01 for 1 second and then off
-	        System.out.println("--> GPIO state should be: ON for only 1 second");
-	        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
+	        Thread.sleep(4000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -52,6 +34,7 @@ public class Controller {
 
         // stop all GPIO activity/threads by shutting down the GPIO controller
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        pin.setShutdownOptions(true, PinState.LOW);
         gpio.shutdown();
 
         System.out.println("Exiting ControlGpioExample");
