47424112080e9d96115ecb76f360537d6f4071cf
Kuhjunge <quhberta@gmail.com>
2018-04-26 10:43:22 +0200

    Umbau auf SPI

--- raspisoftware/src/main/java/raspisoftware/Controller.java
+++ raspisoftware/src/main/java/raspisoftware/Controller.java
@@ -1,54 +1,30 @@
 package raspisoftware;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+import java.io.IOException;
 
 public class Controller {
 
 	public static void main(String[] args) {
-		 // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
-
-        // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput led_pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.HIGH);
-        final GpioPinDigitalInput photo_pin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_28, "MyPhotosensor");
-        //pin.high();
-        //pin.setState(true);
-        // set shutdown state for this pin
-        //pin.setShutdownOptions(true, PinState.LOW);
-        
-
-        try {
-	        long t= System.currentTimeMillis();
-	        long end = t+30000;
-	        while(System.currentTimeMillis() < end) {
-	          // do something
-	          // pause to avoid churning
-	        if(photo_pin.getState() == PinState.HIGH) {
-	        	led_pin.setState(PinState.HIGH);
-	        } else {
-	        	led_pin.setState(PinState.LOW);
-	        }
-	          Thread.sleep( 200 );
-	        }
-
-		} catch (InterruptedException e) {
+		HardwareControl hwc = new HardwareControl();
+		hwc.initLed();
+		//hwc.initLightOnIfDark();
+		
+		try {
+			hwc.initSpi();
+			long t = System.currentTimeMillis();
+			long end = t + 30000;
+			while (System.currentTimeMillis() < end) {
+				// do something
+				// pause to avoid churning
+				hwc.read();
+				Thread.sleep(200);
+			}
+		} catch (InterruptedException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-       
-        // turn off gpio pin #01
-        
+		hwc.shutdown();
+		System.out.println("Exiting ControlGpioExample");
+	}
 
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        led_pin.setShutdownOptions(true, PinState.LOW);
-        gpio.shutdown();
-
-        System.out.println("Exiting ControlGpioExample");
-    }
 }
